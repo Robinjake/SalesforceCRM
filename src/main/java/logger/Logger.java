87b23e7d86e7e574d89f656b7623c1f386590ce1
kyletruong <kyletruong@gmail.com>
2019-03-11 14:48:40 -0700

    add functionality to logger class

--- src/main/java/logger/Logger.java
+++ src/main/java/logger/Logger.java
@@ -1,4 +1,19 @@
 package main.java.logger;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Logger {
+    private Map<Integer, Sale> allSales;
+
+    public Logger() {
+        allSales = new HashMap<>();
+    }
+
+    public offerSale(Sale sale) {
+        allSales.put(sale.getSaleID(), sale);
+        sale.setSaleDecision(sale.getCustomer().acceptSale());
+        sale.setRating(sale.getCustomer().rateTransaction());
+        sale.getEmployee().updateEmployeeSales(sale);
+    }
 }
