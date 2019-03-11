a647e8cd98a810d3a6560302ebeec031b8536657
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2019-03-11 15:01:25 -0700

    add sale fuctionality

---
+++ src/main/java/logger/Sale.java
@@ -0,0 +1,26 @@
+package main.java.logger;
+
+public class Sale
+{
+    int ID;
+    int price;
+    int quantity;
+    String product;
+    Employee employee;
+    Customer customer;
+    int ratingField;
+    boolean SaleDecision;
+    public void setSaleDescision(boolean decision)
+    {
+        SaleDecision = decision;
+    }
+
+    public int getRatingField();
+    public Employee getEmployee;
+    public void setEmployee(Employee employee);
+
+    public Customer getCustomer;
+    public void setCustomer(Customer customer);
+
+}
+
