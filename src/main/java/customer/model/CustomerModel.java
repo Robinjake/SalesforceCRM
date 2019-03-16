257aec1ceecea846cf35fbf7e697af1d690a96a1
Alan Noun <alannoun@hotmail.com>
2019-03-15 22:53:17 -0700

    new test and slight changes to employee and customerModel. Added getID for customer

--- src/main/java/customer/model/CustomerModel.java
+++ src/main/java/customer/model/CustomerModel.java
@@ -40,4 +40,8 @@ public class CustomerModel {
         closedSales.put(closedSale.getId(), closedSale);
         logger.acceptClosedSale(closedSale.getId());
     }
+    
+    public int getID() {
+        return id;
+    }
 }
