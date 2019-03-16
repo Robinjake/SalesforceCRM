8e9e193cf5bd659f76bb9bc8dfa6406b159e898e
kyletruong <kyletruong@gmail.com>
2019-03-15 21:43:51 -0700

    change customer openSales to map

--- src/main/java/customer/model/CustomerModel.java
+++ src/main/java/customer/model/CustomerModel.java
@@ -13,29 +13,29 @@ import java.util.Map;
 
 public class CustomerModel {
     // Member fields
-    public List<Sale> openSales; //TODO: make this a map
+    public Map<Integer, Sale> openSales; //TODO: make this a map
     public Map<Integer, Sale> closedSales;
     Logger logger;
     int id;
 
     // Constructor
-    public CustomerModel(int customer_id) {
-        openSales = new ArrayList<>();
+    public CustomerModel(int customer_id, Logger log) {
+        openSales = new HashMap<>();
         closedSales = new HashMap<>();
-        logger = new Logger();
+        logger = log;
         id = customer_id;
     }
 
     // Retrieve a new sale offer created by an employee
     public void retrieveNewSale(Sale sale) {
-        openSales.add(sale);
+        openSales.put(sale.getId(), sale);
     }
 
     // TODO: update this when openSales is changed to a map
     // Moves a sale from open to closed
     // Notify the logger that the sale has been closed
-    public void closeSale(int sale_index, boolean decision) {
-        Sale closedSale = openSales.remove(sale_index);
+    public void closeSale(int sale_id, boolean decision) {
+        Sale closedSale = openSales.remove(sale_id);
         closedSale.setSaleDecision(decision);
         closedSales.put(closedSale.getId(), closedSale);
         logger.acceptClosedSale(closedSale.getId());
